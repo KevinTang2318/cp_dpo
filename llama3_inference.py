c40ebae4e261f80b7e50d0bf3104a4abc5cdc5b3
Kevin Tang <kevintang2318@gmail.com>
2024-11-15 00:56:11 +0000

    Add inference result for AQuA dataset and corresponding inference code

--- llama3_inference.py
+++ llama3_inference.py
@@ -1,6 +1,8 @@
 from huggingface_hub import login
+import json
 import transformers
 import torch
+import tqdm
 
 from data_loader import load_aqua_data, load_strategy_qa_data
 
@@ -8,7 +10,7 @@ from data_loader import load_aqua_data, load_strategy_qa_data
 def generate_aqua_constrastive_prompt(sample):
     prompt = f"""
         Q: {sample["question"]} Choose one correct answer from the following  options: {sample["options"]}.
-        A: Let's give a correct and a wrong answer.
+        A: Let's give a correct and a wrong answer. Output your answer in the following format: "Correct answer: <correct letter choice>; Wrong answer: <wrong letter choice>; <your reasoning>
     """
 
     return prompt
@@ -17,7 +19,7 @@ def generate_aqua_constrastive_prompt(sample):
 def generate_strategy_qa_constrastive_prompt(sample):
     prompt = f"""
         Q: {sample["input"]} Answer this question with either "Yes." or "No." first, then provide your reasoning.
-        A: Let's give a correct and a wrong answer.
+        A: Let's give a correct and a wrong answer. Output your answer in the following format: "Correct answer: <yes or no>; Wrong answer: <yes or no>; <your reasoning>
     """
 
     return prompt
@@ -34,20 +36,35 @@ if __name__ == "__main__":
 
     pipeline = transformers.pipeline(
         "text-generation",
-        model="meta-llama/Meta-Llama-3-8B-Instruct",
+        model=model_id,
         model_kwargs={"torch_dtype": torch.bfloat16},
-        device="cuda",
-        num_return_sequences=1,
+        device="cuda"
     )
-    tokenizer = transformers.AutoTokenizer.from_pretrained(
-        "meta-llama/Meta-Llama-3-8B-Instruct"
-    )
-    eos_token_id = tokenizer.eos_token_id
+    
+    terminators = [
+        pipeline.tokenizer.eos_token_id,
+        pipeline.tokenizer.convert_tokens_to_ids("<|eot_id|>")
+    ]
 
-    print(generate_aqua_constrastive_prompt(aqua_dataset[0]))
+    # generate llm output form AQuA dataset:
+    aqua_output = []
+    for sample in tqdm.tqdm(aqua_dataset):
+        prompt = generate_aqua_constrastive_prompt(sample)
 
-    print(pipeline(
-        generate_aqua_constrastive_prompt(aqua_dataset[0]),
-        eos_token_id=eos_token_id,
-    )
-    )
+        messages=[
+            {"role": "system", "content": "Assistant is a large language model."},
+            {"role": "user", "content": prompt}
+        ]
+
+        outputs = pipeline(
+            messages,
+            max_new_tokens=1024,
+            eos_token_id=terminators,
+            temperature=0.01,
+        )
+
+        sample["llama_output"] = outputs[0]["generated_text"][-1]["content"]
+        aqua_output.append(sample)
+
+    with open("llm_output/aqua_output.json", 'w') as json_file:
+        json.dump(aqua_output, json_file, indent=4)
