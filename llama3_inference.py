5f5c30ef888174b4e33bc2eb06e5622027581409
KevinTang2318 <kevintang2318@gmail.com>
2024-12-14 01:00:00 +0000

    Perform train test split while doing initial inference to ensure the consistency of data throught the process

--- llama3_inference.py
+++ llama3_inference.py
@@ -1,17 +1,5 @@
-import argparse
 import json
-import transformers
-import torch
 import tqdm
-
-from huggingface_hub import login
-
-from data_loader import (
-    load_aqua_data, 
-    load_strategy_qa_data, 
-    load_coin_flip_data, 
-    load_object_tracking_data
-)
 
 
 def generate_aqua_prompt(sample):
@@ -21,16 +9,45 @@ def generate_aqua_prompt(sample):
 
     return prompt
 
-def generate_object_tracking_prompt(sample):
+
+def generate_aqua_constrastive_prompt(sample):
     prompt = f"""
-        Q: {sample["input"]} Choose one correct answer from the following  options: {",".join(list(sample["target_scores"].keys()))}.Output your answer in the following format: Answer: <letter choice>; <your reasoning>
+        Q: {sample["question"]} Choose one correct answer from the following  options: {sample["options"]}.
+        A: Let's give a correct and a wrong answer. Output your answer in the following format: "Correct answer: <correct letter choice>; Wrong answer: <wrong letter choice>; <your reasoning>
     """
 
     return prompt
 
+
+def generate_object_tracking_prompt(sample):
+    prompt = f"""
+        Q: {sample["input"]} Choose one correct answer from the following  options: {",".join(list(sample["target_scores"].keys()))}.Output your answer in the following format: Answer: <your choice>; <your reasoning>
+    """
+
+    return prompt
+
+
+def generate_object_tracking_constrastive_prompt(sample):
+    prompt = f"""
+        Q: {sample["input"]} Choose one correct answer from the following  options: {",".join(list(sample["target_scores"].keys()))}.
+        A: Let's give a correct and a wrong answer. Output your answer in the following format: "Correct answer: <correct ball color>; Wrong answer: <correct ball color>; <your reasoning>
+    """
+
+    return prompt
+
+
 def generate_strategy_qa_prompt(sample):
     prompt = f"""
+        Q: {sample["input"]}; Output your answer in the following format: Answer: <YES or NO>; <your reasoning>
+    """
+
+    return prompt
+
+
+def generate_strategy_qa_constrastive_prompt(sample):
+    prompt = f"""
         Q: {sample["input"]} Answer this question with either "Yes." or "No." first, then provide your reasoning.
+        A: Let's give a correct and a wrong answer. Output your answer in the following format: "Correct answer: <yes or no>; Wrong answer: <yes or no>; <your reasoning>
     """
 
     return prompt
@@ -38,7 +55,17 @@ def generate_strategy_qa_prompt(sample):
     
 def generate_coin_flip_prompt(sample):
     prompt = f"""
+        Q: {sample["question"]}; Output your answer in the following format: 
+        Answer: <YES or NO>; <your reasoning>
+    """
+
+    return prompt
+
+    
+def generate_coin_flip_constrastive_prompt(sample):
+    prompt = f"""
         Q: {sample["question"]} Answer this question with either "Yes." or "No." first, then provide your reasoning.
+        A: Let's give a correct and a wrong answer. Output your answer in the following format: "Correct answer: <yes or no>; Wrong answer: <yes or no>; <your reasoning>
     """
 
     return prompt
@@ -73,53 +100,3 @@ def inference(dataset, prompt_generator, pipeline, terminators, out_file_name):
 
     with open(f"llm_output/{out_file_name}", 'w') as json_file:
         json.dump(all_output, json_file, indent=4)
-
-
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(
-        description="Run inference on selected tasks.")
-    parser.add_argument(
-        "tasks",
-        nargs="+",
-        choices=["aqua", "strategy_qa", "coin_flip", "object_tracking"],
-        help="Specify one or more tasks to run inference on."
-    )
-    args = parser.parse_args()
-
-    datasets = {
-        "aqua": (load_aqua_data("datasets/AQuA/test.json"), generate_aqua_prompt, "aqua_output.json"),
-        "strategy_qa": (load_strategy_qa_data("datasets/StrategyQA/task.json"), generate_strategy_qa_prompt, "strategy_qa_output.json"),
-        "coin_flip": (load_coin_flip_data("datasets/CoinFlip/coin_flip.json"), generate_coin_flip_prompt, "coin_flip_output.json"),
-        "object_tracking": (load_object_tracking_data("datasets/BigBench_Object_Tracking/task.json"), generate_object_tracking_prompt, "object_tracking_output.json"),
-    }
-
-    login(token="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")
-
-    model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
-
-    pipeline = transformers.pipeline(
-        "text-generation",
-        model=model_id,
-        model_kwargs={"torch_dtype": torch.bfloat16},
-        device="cuda"
-    )
-    
-    terminators = [
-        pipeline.tokenizer.eos_token_id,
-        pipeline.tokenizer.convert_tokens_to_ids("<|eot_id|>")
-    ]
-
-    for task in args.tasks:
-        if task in datasets:
-            dataset, prompt_generator, output_file = datasets[task]
-            print(f"Running inference on {task} dataset.")
-            inference(
-                dataset,
-                prompt_generator,
-                pipeline,
-                terminators,
-                output_file
-            )
-        else:
-            print(f"Task '{task}' is not recognized. Skipping.")
